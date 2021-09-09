74756003fddf28eb4957e736a00c5c03d40c4895
astephens-fsd <astephens.pm@icloud.com>
2021-09-08 19:16:17 -0700

    completed Task 7

--- index.js
+++ index.js
@@ -187,9 +187,17 @@ Use the filterByWord function below to do the following:
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
+function filterByWord(array, chocolate){
+  let newChocoArray = [];
+  for(let i =0; i < array.length; i++){
+    if(array[i].includes(chocolate)){
+      newChocoArray.push(array[i]);
+    }
+  }
+  return newChocoArray;
 }
+
+console.log(filterByWord(originalFlavors, 'Chocolate'));
 
 
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
